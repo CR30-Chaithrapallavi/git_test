12c2b37047264f189c096dff924927ec8240eb51
Chaithrapallavi <p.chaithra@globaledgesoft.com>
2021-02-17 10:12:02 +0530

    added exp.c

---
+++ exp.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+void f(char **p)
+{
+	char *t;
+	t = (p += sizeof(int))[-1];
+	printf("%s",t);
+}
+int main()
+{
+	char *a[]= {"ab", "cd", "ef", "gh", "ij","kl"};
+	f(a);
+	return 0;
+}
